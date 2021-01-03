93f9ade5c30a98c56d1cd9e307cfcf2e532c5f50
Muhammad Ayaz Hanif <68582699+mayazhanif@users.noreply.github.com>
2021-01-03 17:11:48 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","coversion_emoji","Emoji@123","coversion_emoji");
+$con = mysqli_connect("localhost","test","test","test");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
